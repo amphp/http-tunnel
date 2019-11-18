97ed2b267d663cbf066fb2fc9c904cd378fc4451
Niklas Keller <me@kelunik.com>
2019-11-18 20:11:22 +0100

    Extract from amphp/http-client

---
+++ README.md
@@ -0,0 +1,31 @@
+# http-tunnel
+
+[![Build Status](https://img.shields.io/travis/amphp/http-tunnel/master.svg?style=flat-square)](https://travis-ci.org/amphp/http-tunnel)
+[![CoverageStatus](https://img.shields.io/coveralls/amphp/http-tunnel/master.svg?style=flat-square)](https://coveralls.io/github/amphp/http-tunnel?branch=master)
+![License](https://img.shields.io/badge/license-MIT-blue.svg?style=flat-square)
+
+This package provides an HTTP `CONNECT` tunnel for PHP based on [Amp](https://github.com/amphp/amp).
+
+## Installation
+
+This package can be installed as a [Composer](https://getcomposer.org/) dependency.
+
+```bash
+composer require amphp/http-tunnel
+```
+
+## Examples
+
+More extensive code examples reside in the [`examples`](./examples) directory.
+
+## Versioning
+
+`amphp/http-tunnel` follows the [semver](http://semver.org/) semantic versioning specification like all other `amphp` packages.
+
+## Security
+
+If you discover any security related issues, please email [`me@kelunik.com`](mailto:me@kelunik.com) instead of using the issue tracker.
+
+## License
+
+The MIT License (MIT). Please see [`LICENSE`](./LICENSE) for more information.
---
+++ examples/http-client-via-proxy.php
@@ -0,0 +1,71 @@
+<?php
+
+use Amp\Http\Client\Connection\UnlimitedConnectionPool;
+use Amp\Http\Client\HttpClientBuilder;
+use Amp\Http\Client\HttpException;
+use Amp\Http\Client\Request;
+use Amp\Http\Client\Response;
+use Amp\Http\Rfc7230;
+use Amp\Http\Tunnel\Http1TunnelConnector;
+use Amp\Loop;
+use Amp\Socket\SocketAddress;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+Loop::run(static function () use ($argv) {
+    try {
+        // If you need authentication, you can set a custom header (using Basic auth here)
+        // $connector = new Http1Tunnel(new SocketAddress('127.0.0.1', 5512), [
+        //     'proxy-authorization' => 'Basic ' . \base64_encode('user:pass'),
+        // ]);
+
+        // If you have a proxy accepting HTTPS connections, you need to use Https1Tunnel instead:
+        // $connector = new Https1Tunnel(new SocketAddress('proxy.example.com', 5512));
+        $connector = new Http1TunnelConnector(new SocketAddress('127.0.0.1', 5512));
+
+        $client = (new HttpClientBuilder)
+            ->usingPool(new UnlimitedConnectionPool($connector))
+            ->build();
+
+        $request = new Request('http://amphp.org/');
+
+        /** @var Response $response */
+        $response = yield $client->request($request);
+
+        $request = $response->getRequest();
+
+        \printf(
+            "%s %s HTTP/%s\r\n",
+            $request->getMethod(),
+            $request->getUri(),
+            \implode('+', $request->getProtocolVersions())
+        );
+
+        /** @noinspection PhpUnhandledExceptionInspection */
+        print Rfc7230::formatHeaders($request->getHeaders()) . "\r\n\r\n";
+
+        \printf(
+            "HTTP/%s %d %s\r\n",
+            $response->getProtocolVersion(),
+            $response->getStatus(),
+            $response->getReason()
+        );
+
+        /** @noinspection PhpUnhandledExceptionInspection */
+        print Rfc7230::formatHeaders($response->getHeaders()) . "\r\n\r\n";
+
+        $body = yield $response->getBody()->buffer();
+        $bodyLength = \strlen($body);
+
+        if ($bodyLength < 250) {
+            print $body . "\r\n";
+        } else {
+            print \substr($body, 0, 250) . "\r\n\r\n";
+            print($bodyLength - 250) . " more bytes\r\n";
+        }
+    } catch (HttpException $error) {
+        // If something goes wrong Amp will throw the exception where the promise was yielded.
+        // The HttpClient::request() method itself will never throw directly, but returns a promise.
+        echo $error;
+    }
+});
